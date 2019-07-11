6aedc960e0b807196c37b9a1fb76d6e2533f21e5
DYBCyrus <yad025@ucsd.edu>
2019-07-10 17:52:16 -0700

    autocomplete completed

--- NetflixPrize3/NetflixPrize3/views.py
+++ NetflixPrize3/NetflixPrize3/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import pandas as pd
+import os
+import json
 
 def button(request):
-    return render(request, 'home.html')
+    # print(os.getcwd())
+    df = pd.read_csv('IMDB_Final_Movies.csv', delimiter=',')
+    title = df['primaryTitle'].tolist()
+    year = df['startYear'].tolist()
+    titles = []
+    for (i,j) in zip(title,year):
+        if not np.isnan(j):
+            titles.append(i + '(' + str(int(j)) + ')')
+        else:
+            titles.append(i + '(N/A)')
+    print(titles[0])
+    return render(request, 'home.html', {'titles': titles})
 
