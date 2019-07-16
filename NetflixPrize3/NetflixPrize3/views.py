cc455a72877f719d035ac2416aaf0ee9894c7874
springunicorn <38194197+springunicorn@users.noreply.github.com>
2019-07-15 18:13:00 -0700

    (⊙﹏⊙)

--- NetflixPrize3/NetflixPrize3/views.py
+++ NetflixPrize3/NetflixPrize3/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 import numpy as np
 import matplotlib.pyplot as plt
 import random
@@ -6,8 +7,11 @@ import pandas as pd
 import os
 import json
 
+
+
 def button(request):
     # print(os.getcwd())
+    global df
     df = pd.read_csv('IMDB_Final_Movies.csv', delimiter=',')
     title = df['primaryTitle'].tolist()
     year = df['startYear'].tolist()
@@ -20,3 +24,15 @@ def button(request):
     print(titles[0])
     return render(request, 'home.html', {'titles': titles})
 
+def fetchFeatures(request):
+    longTitle = request.POST.get('movie_title', False)
+    title = ""
+    if longTitle:
+        title = longTitle.split('(')
+        year = int(title[1].split(')')[0])
+        title = title[0]
+    # Search the movie entry using the title and the startYear
+    movieEntry = df.query('primaryTitle == title and startYear == year')
+    directors = movieEntry.iloc[0]["directors_names"]
+    # return JsonResponse({"directors":directors})
+    return render(request, "home.html", {"directors",directors})
