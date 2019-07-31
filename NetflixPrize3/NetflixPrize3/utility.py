fc7f69c3c2eace4951fa3fde6dc124ed099586cf
DYBCyrus <yad025@ucsd.edu>
2019-07-31 11:53:01 -0700

    adjust figure size

--- NetflixPrize3/NetflixPrize3/utility.py
+++ NetflixPrize3/NetflixPrize3/utility.py
@@ -11,8 +11,9 @@ def plot_features(sortedLogFeat):
         coefs.append(sortedLogFeat[i][1])
         i += 1
     y_pos = np.arange(len(xlabels))
+    plt.figure(figsize=(10,12))
     plt.bar(y_pos, coefs, align='center', alpha=0.5)
-    plt.xticks(y_pos, xlabels)
+    plt.xticks(y_pos, xlabels, rotation=45)
     plt.ylabel('Feature Coefficient')
     plt.title('Main Contributing Features')
 
